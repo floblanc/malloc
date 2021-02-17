eb7055a6af12645a0a44d6223e544ba7a305be33
Florian Blanchard <florian5611@gmail.com>
2021-02-17 15:08:30 +0100

    ue

--- compute_mac.sh
+++ compute_mac.sh
@@ -1,5 +1,20 @@
-test0=`./run_mac.sh /usr/bin/time -l ./test0 2>&1 | grep " maximum resident set size" | cut -d " " -f-5`
-test1=`./run_mac.sh /usr/bin/time -l ./test1 2>&1 | grep " maximum resident set size" | cut -d " " -f-5`
-div=$(getconf PAGESIZE)
-
-echo "($test1 - $test0) / $div" | bc
+#!/bin/bash
+make re -s > /dev/null 2>&1
+read -p 'How many iterations: ' it
+j=0
+i=0
+while [ $i -lt $it ]
+do
+	test0=`./run_mac.sh /usr/bin/time -l ./test0 2>&1 | grep "page reclaims" | cut -d " " -f18`
+	test1=`./run_mac.sh /usr/bin/time -l ./test1 2>&1 | grep "page reclaims" | cut -d " " -f18`
+	ret=`echo "$test1 - $test0" | bc`
+	if [ $ret -gt 254 -a $ret -lt 273 ]
+	then
+		j=$(($j + 1))
+	else
+		echo "\n$ret"
+	fi
+	printf "\rValue:\t$ret\tValid : $j/$it"
+	i=$(($i + 1))
+done
+echo "\n$j/$it passed the condition"
